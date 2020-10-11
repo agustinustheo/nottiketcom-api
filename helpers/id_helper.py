b8a95f7514086e657d4184fc102aa96b8b347d46
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-10-11 08:42:11 +0700

    fix: bugfix on generate_id method and bot_service.

--- helpers/id_helper.py
+++ helpers/id_helper.py
@@ -1,6 +1,7 @@
 from random import randrange
 
-def generate_id():
+def generate_id(length):
+    result = ""
     characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     characters_length = len(characters)
     for x in range(length):
--- services/bot_service.py
+++ services/bot_service.py
@@ -2,6 +2,7 @@ import os
 import json
 from telegram import Bot
 from flask import request
+from helpers.id_helper import generate_id
 from helpers.user_helper import get_user_by_telegram_id
 from helpers.telegram_helper import create_telegram, get_telegram_by_id
 
@@ -18,19 +19,20 @@ def start(update):
         bot = get_bot()
         try:
             res = get_user_by_telegram_id(update["message"]["from"]["id"])
-            message = f"Welcome back {res["username"]}, glad to see you! My name is Wowo and I will be your helper bot. I can help you find concert tickets if you want? You an also command me to do some other stuff using these commands:\n\n/help\n/concert"
+            message = f'Welcome back {res["username"]}, glad to see you! My name is Wowo and I will be your helper bot. I can help you find concert tickets if you want? You an also command me to do some other stuff using these commands:\n\n/help\n/concert'
             bot.send_message(chat_id=update["message"]["chat"]["id"], text=message)
         except:
             try:
                 res = get_telegram_by_telegram_id(update["message"]["from"]["id"])
             except:
                 body = {}
-                body["otp"] = update
+                body["otp"] = generate_id(6)
                 body["chat_id"] = update["message"]["chat"]["id"]
                 body["telegram_id"] = update["message"]["from"]["id"]
+                print(body)
                 res = create_telegram(body)
             
-            message = f"Welcome {update["message"]["from"]["username"]} My name is Wowo and I will be your helper bot. I need to validate who you are, so please enter this code {res["otp"]} when you login/register using your nottiketcom account.\nAccess your account here https://nottiketcom.xyz/login"
+            message = f'Welcome {update["message"]["from"]["username"]} My name is Wowo and I will be your helper bot. I need to validate who you are, so please enter this code {res["otp"]} when you login/register using your nottiketcom account.\n\nAccess your account here https://nottiketcom.xyz/login'
             bot.send_message(chat_id=update["message"]["chat"]["id"], text=message)
     except Exception as ex:
         print(ex)
@@ -41,10 +43,10 @@ def help_(update):
         bot = get_bot()
         try:
             res = get_user_by_telegram_id(update["message"]["from"]["id"])
-            message = f"Welcome back {res["username"]}, glad to see you! My name is Wowo and I will be your helper bot. I can help you find concert tickets if you want? You an also command me to do some other stuff using these commands:\n\n/help\n/concert"
+            message = f'Welcome back {res["username"]}, glad to see you! My name is Wowo and I will be your helper bot. I can help you find concert tickets if you want? You an also command me to do some other stuff using these commands:\n\n/help\n/concert'
             bot.send_message(chat_id=update["message"]["chat"]["id"], text=message)
         except:
-            message = f"Sorry {update["message"]["from"]["username"]} you have not registered this account please chat /start to begin registration"
+            message = f'Sorry {update["message"]["from"]["username"]} you have not registered this account please chat /start to begin registration'
             bot.send_message(chat_id=update["message"]["chat"]["id"], text=message)
     except Exception as ex:
         print(ex)
