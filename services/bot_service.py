907edf62fcd87de40f77e9c5486a6dae65ccdcb7
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-10-11 09:32:03 +0700

    fix: added lambda to default result.

--- services/bot_service.py
+++ services/bot_service.py
@@ -62,6 +62,6 @@ def bot_start():
         "/start": start,
         "/help": help_
     }
-    func = switcher.get(received_message, help)
+    func = switcher.get(received_message, lambda: help)
     func(update)
     return "Message sent!"
